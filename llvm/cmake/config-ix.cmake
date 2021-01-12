868afc62698a9cc0744340242461c7480e0b01be
Home <home@localhost.localdomain>
2021-01-12 14:20:10 -0500

    llvm: do not detect host triple

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -346,12 +346,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
   endif()
 endif()
 
-# By default, we target the host, but this can be overridden at CMake
-# invocation time.
-include(GetHostTriple)
-get_host_triple(LLVM_INFERRED_HOST_TRIPLE)
-
-set(LLVM_HOST_TRIPLE "${LLVM_INFERRED_HOST_TRIPLE}" CACHE STRING
+set(LLVM_HOST_TRIPLE "x86-unknown-linux-musl" CACHE STRING
     "Host on which LLVM binaries will run")
 
 # Determine the native architecture.
